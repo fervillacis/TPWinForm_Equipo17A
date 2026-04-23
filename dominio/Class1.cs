41f0e6215f3247e84285d29ddb2ec6c77cc44c03
Patricio Botello <pbotello193@gmail.com>
2026-04-23 20:01:41 -0300

    Clase Articulo

--- dominio/Class1.cs
+++ dominio/Class1.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace dominio
 {
     public class Class1
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public string UrlImagen { get; set; }
+
+        public List<string> Imagenes { get; set; }
+
     }
 }
